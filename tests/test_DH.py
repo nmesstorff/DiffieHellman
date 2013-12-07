140f709929093dc3d52fb56cfe51bae74800cade
Norman Meßtorff <normes@normes.org>
2013-12-07 22:45:25 +0100

    remove long literal 'L' to be python 3 compatible

--- tests/test_DH.py
+++ tests/test_DH.py
@@ -12,16 +12,16 @@ class TestDH(unittest.TestCase):
         self.bob.prvsecret = 19
 
     def test_gen_pubkey(self):
-        self.assertEqual(self.alice.gen_pubkey(), 486L)
-        self.assertEqual(self.bob.gen_pubkey(), 1876L)
+        self.assertEqual(self.alice.gen_pubkey(), 486)
+        self.assertEqual(self.bob.gen_pubkey(), 1876)
 
     def test_gen_shared_secret(self):
-        self.assertEqual(self.alice.gen_shared_secret(1876L), 956L)
-        self.assertEqual(self.bob.gen_shared_secret(486L), 956L)
+        self.assertEqual(self.alice.gen_shared_secret(1876), 956)
+        self.assertEqual(self.bob.gen_shared_secret(486), 956)
 
     def test_set_shared_secret(self):
-        self.alice.set_shared_secret(1876L)
-        self.bob.set_shared_secret(486L)
+        self.alice.set_shared_secret(1876)
+        self.bob.set_shared_secret(486)
         self.assertEqual(self.alice.sharedsecret, self.bob.sharedsecret)
 
     def test_DH_init(self):
